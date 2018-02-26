3151c0aa890908847b4d7dd45f708e6bdd7b793c
cj <thatcat13@gmail.com>
2018-02-25 16:23:30 -0800

    apply thunk middleware and implement second API call

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,13 +25,45 @@ export function fetchSongId(title) {
       response => response.json(),
       error => console.log('An error occurred.', error)
     ).then(function(json) {
-      console.log('CHECK OUT THIS SWEET API RESPONSE:', json);
+      if (json.message.body.track_list.length > 0 ) {
+        const musicMatchId = json.message.body.track_list[0].track.track_id;
+        const artist = json.message.body.track_list[0].track.artist_name;
+        const title = json.message.body.track_list[0].track.track_name;
+        fetchLyrics(title, artist, musicMatchId, localSongId, dispatch);
+      } else {
+        console.log('we couldn\'t locate a song under that ID!');
+      }
     });
   };
+}
+
+export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch) {
+  return fetch('http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + musicMatchId + '&apikey=94114939e5b1aa341e4815685bdb0b60').then(
+    response => response.json(),
+    error => console.log('An error occurred.', error)
+  ).then(function(json) {
+    if (json.message.body.lyrics) {
+      let lyrics = json.message.body.lyrics.lyrics_body;
+      lyrics = lyrics.replace('"', '');
+      const songArray = lyrics.split(/\n/g).filter(entry => entry!="");
+      dispatch(receiveSong(title, artist, localSongId, songArray));
+    } else {
+      console.log('couldn\'t locate lyrics oh noes!');
+    }
+  });
 }
 
 export const requestSong = (title, localSongId) => ({
   type: types.REQUEST_SONG,
   title,
   songId: localSongId
+});
+
+export const receiveSong = (title, artist, songId, songArray) => ({
+  type: types.RECEIVE_SONG,
+  songId,
+  title,
+  artist,
+  songArray,
+  receivedAt: Date.now()
 });
